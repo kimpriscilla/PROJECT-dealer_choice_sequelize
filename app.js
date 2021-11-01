5d40e3f199230312c6a7059ca222beaf324d83c9
Priscilla <priscillakim58@gmail.com>
2021-11-01 01:16:03 -0400

    detail page

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const app = express();
 
+const books = ["Book 1", "Book 2", "Book 3"];
+
 app.get("/", (req, res) => {
-  res.send("hi world");
+  res.send(`<html>
+    <head>
+    </head>
+      <body>
+        <h1>Self-Help Books</h1>
+        <ul>
+        ${books
+          .map((book, idx) => {
+            return `<li>
+            <a href ='/details/${idx}'>${book}</a>
+          </li>`;
+          })
+          .join("")}
+        </ul>
+      </body>
+  </html>`);
+});
+
+app.get("/details/:id", (req, res) => {
+  const book = books[req.params.id];
+  res.send(
+    `  <html>
+      <head></head>
+      <body>
+      <a href = '/'> All Books </a>
+        <h1>Details for ${book}</h1>
+      </body>
+    </html>`
+  );
 });
 
 const PORT = 1337;
