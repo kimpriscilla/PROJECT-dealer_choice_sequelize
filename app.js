5b4d33b6a1540868f79e0c5079fc17f5e2f21c63
Priscilla <priscillakim58@gmail.com>
2021-11-02 21:45:47 -0400

    redid main & each page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const path = require("path");
+const data = require("./data");
 
 const books = ["Cat 1", "Cat 2", "Cat 3"];
 
@@ -9,34 +10,56 @@ app.use(morgan("dev"));
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send(`<html>
+  const cats = data.list();
+  const html = `<!DOCTYPE html>
+  <html>
   <head>
-  <link rel='stylesheet' href='/public/styles.css'/>
+    <title>Wizard News</title>
+    <link rel='stylesheet' href='/public/styles.css'/>
   </head>
-    <body>
-      <h1>Breeds of Cats</h1>
-      <nav>
-      ${books
-        .map((book, idx) => {
-          return `
-          <a href ='/details/${idx}'>${book}</a>
-        `;
-        })
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>BREED OF CATS</header>
+      ${cats
+        .map(
+          (cat) => `
+        <div class='news-item'>
+          <p>
+          <a href='/cats/${cat.id}'><span class="news-position">${cat.id}. </span>${cat.breed}
+          </p>
+        </div>`
+        )
         .join("")}
-      </nav>
-    </body>
-  </html>`);
+    </div>
+  </body>
+</html>`;
+  res.send(html);
 });
 
-app.get("/details/:id", (req, res) => {
-  const book = books[req.params.id];
+app.get("/cats/:id", (req, res) => {
+  const id = req.params.id;
+  const cat = data.find(id);
   res.send(
-    `  <html>
-      <head></head>
-      <body>
-      <a href = '/'> All Books </a>
-        <h1>Details for ${book}</h1>
-      </body>
+    `<!DOCTYPE html>
+    <html>
+    <head>
+
+    <link rel='stylesheet' href='/public/styles.css'/>
+
+    </head>
+    <body>
+      <div class="news-list">
+      <a href='/'> <header>HOME</header>
+
+          <div class='news-item'>
+            <p>
+             <span class="news-position">${cat.breed} </span>
+
+            </p>
+            <p>${cat.fact}</p>
+          </div>
+      </div>
+    </body>
     </html>`
   );
 });
